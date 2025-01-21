ace9012714a95738bdddabde451626cdd250e137
zimfite <katy2007dv@gmail.com>
2025-01-21 14:28:30 +0300

    main page

--- app/urls.py
+++ app/urls.py
@@ -1,1 +1,7 @@
-urlpatterns = []
+from django.urls import path
+from .views import *
+
+urlpatterns = [
+    path('hello_world/', hello_world, name='hello_world'),
+    path('', home, name='home')
+]
--- app/views.py
+++ app/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def hello_world(request):
+    return HttpResponse('Привет мир!')
+
+def home(request):
+    return render(request, 'home.html')
